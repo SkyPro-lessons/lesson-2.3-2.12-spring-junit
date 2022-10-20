726887a6dc07c85702285ad9dde158b3ab56c196
Tarasov Sergei <www.neon@mail.ru>
2022-10-20 08:34:46 +0500

    Initial commit

---
+++ src/main/java/site/telion/lesson_2_32_12springjunit/Application.java
@@ -0,0 +1,13 @@
+package site.telion.lesson_2_32_12springjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
